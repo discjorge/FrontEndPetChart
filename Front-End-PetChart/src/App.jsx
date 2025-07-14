b5386666de0a6ab22eeb6c389ef4f6d471b783ea
Jorge <jorgeantoniobarrios@gmail.com>
2025-07-13 22:12:23 -0500

    appointment dashboard

--- Front-End-PetChart/src/App.jsx
+++ Front-End-PetChart/src/App.jsx
@@ -1,18 +1,19 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './Components/AuthContent.jsx';
-import ProtectedRoute from './Components/ProtectedRoute.jsx';
-import NavBar from './Components/NavBar.jsx';
-import Home from './Components/Home.jsx';
-import LoginPage from './Components/LoginPage.jsx';
-import Register from './Components/Register.jsx';
-import PetParentRegister from './Components/PetParentRegister.jsx';
-import VetRegister from './Components/VetRegister.jsx';
-import PetParentDashboard from './Components/PetParentDashboard.jsx';
-import VeterinarianDashboard from './Components/VetDashboard.jsx';
-import Account from './Components/Account.jsx';
-import ComingSoon from './Components/ComingSoon.jsx';
-import ManagePatients from './Components/ManagePatients.jsx';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./Components/AuthContent.jsx";
+import ProtectedRoute from "./Components/ProtectedRoute.jsx";
+import NavBar from "./Components/NavBar.jsx";
+import Home from "./Components/Home.jsx";
+import LoginPage from "./Components/LoginPage.jsx";
+import Register from "./Components/Register.jsx";
+import PetParentRegister from "./Components/PetParentRegister.jsx";
+import VetRegister from "./Components/VetRegister.jsx";
+import PetParentDashboard from "./Components/PetParentDashboard.jsx";
+import VeterinarianDashboard from "./Components/VetDashboard.jsx";
+import Account from "./Components/Account.jsx";
+import ComingSoon from "./Components/ComingSoon.jsx";
+import ManagePatients from "./Components/ManagePatients.jsx";
+import AppointmentsDashboard from "./Components/AppointmentsDashboard.jsx";
+import "./App.css";
 
 function App() {
   return (
@@ -25,46 +26,73 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/register/pet-parent" element={<PetParentRegister />} />
           <Route path="/register/veterinarian" element={<VetRegister />} />
-          
+
           {/* Protected routes */}
-          <Route path="/dashboard/pet-parent/*" element={
-            <ProtectedRoute allowedUserTypes={['pet-parent']}>
-              <div>
-                <NavBar />
-                <PetParentDashboard />
-              </div>
-            </ProtectedRoute>
-          } />
-          
-          <Route path="/dashboard/veterinarian/*" element={
-            <ProtectedRoute allowedUserTypes={['veterinarian']}>
-              <div>
-                <NavBar />
-                <VeterinarianDashboard />
-              </div>
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/dashboard/pet-parent/*"
+            element={
+              <ProtectedRoute allowedUserTypes={["pet-parent"]}>
+                <div>
+                  <NavBar />
+                  <PetParentDashboard />
+                </div>
+              </ProtectedRoute>
+            }
+          />
 
-          <Route path="/account" element={
-            <ProtectedRoute allowedUserTypes={['pet-parent', 'veterinarian']}>
-              <div>
-                <NavBar />
-                <Account /> 
-              </div>
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/dashboard/veterinarian/*"
+            element={
+              <ProtectedRoute allowedUserTypes={["veterinarian"]}>
+                <div>
+                  <NavBar />
+                  <VeterinarianDashboard />
+                </div>
+              </ProtectedRoute>
+            }
+          />
 
-          <Route path="/coming-soon" element={
-            <ProtectedRoute allowedUserTypes={['pet-parent', 'veterinarian']}>
-              <ComingSoon />
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute allowedUserTypes={["pet-parent", "veterinarian"]}>
+                <div>
+                  <NavBar />
+                  <Account />
+                </div>
+              </ProtectedRoute>
+            }
+          />
 
-          <Route path="/manage-patients" element={
-            <ProtectedRoute allowedUserTypes={['veterinarian']}>
-              <ManagePatients />
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/coming-soon"
+            element={
+              <ProtectedRoute allowedUserTypes={["pet-parent", "veterinarian"]}>
+                <ComingSoon />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/manage-patients"
+            element={
+              <ProtectedRoute allowedUserTypes={["veterinarian"]}>
+                <ManagePatients />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/appointments-dashboard"
+            element={
+              <ProtectedRoute allowedUserTypes={["veterinarian"]}>
+                <div>
+                  <NavBar />
+                  <AppointmentsDashboard />
+                </div>
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </AuthProvider>
